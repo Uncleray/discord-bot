7b843f34525b1a0d4b78049819ec70e1cc39cc96
Uncleray <uncleray@lostwoods.me>
2023-11-05 00:24:28 +0100

    changed to use dotenv

--- bot/settings.py
+++ bot/settings.py
@@ -1,13 +1,9 @@
 # imports
 import os
+from dotenv import load_dotenv
 
-# read discord token from .env file
-with open('.env', 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        key, value = line.strip().split('=')
-        if key == 'DISCORD_BOT_TOKEN':
-            os.environ['DISCORD_BOT_TOKEN'] = value
+load_dotenv()
 
-# get the token from the environment variable
-discord_api_secret = os.getenv('DISCORD_BOT_TOKEN')
+# set value to variables from .env file
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+PREFIX = os.getenv('DISCORD_BOT_PREFIX')
