6d9e91a8efc97a2c332364d8aa2eebb1634d205b
Uncleray <uncleray@lostwoods.me>
2023-11-05 17:39:43 +0100

    changed messages

--- bot/main.py
+++ bot/main.py
@@ -60,22 +60,22 @@ async def on_command_error(ctx, error):
     else:
         print(f'Error: {error}')
 
-# event to to send message to a channel when member joins the discord server
+# event to send message to a channel when member joins the discord server
 @bot.event
 async def on_member_join(member):
     channel = member.guild.system_channel
     if channel:
-        welcome_message = f'{member.mention} just joined the Server!'
+        welcome_message = f'Welcome {member.mention} to this server!'
         await channel.send(welcome_message)
     else:
         print(f'No System Messages Channel was found.')
 
-# event to to send message to a channel when member leaves the discord server
+# event to send message to a channel when member leaves the discord server
 @bot.event
 async def on_member_remove(member):
     channel = member.guild.system_channel
     if channel:
-        goodbye_message = f'{member.mention} just left the server!'
+        goodbye_message = f'Goodbye {member.mention}!'
         await channel.send(goodbye_message)
     else:
         print(f'No System Messages Channel was found.')
