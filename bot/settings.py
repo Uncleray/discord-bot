d6ee3db03300bf135d482bc24cc4100e7f1b954c
Uncleray <uncleray@lostwoods.me>
2023-11-05 01:35:23 +0100

    added variables for welcome and goodbye channel

--- bot/settings.py
+++ bot/settings.py
@@ -7,3 +7,5 @@ load_dotenv()
 # set value to variables from .env file
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 PREFIX = os.getenv('DISCORD_BOT_PREFIX')
+WELCOME_CHANNEL = os.getenv('DISCORD_WELCOME_CHANNEL')
+GOODBYE_CHANNEL = os.getenv('DISCORD_GOODBYE_CHANNEL')
